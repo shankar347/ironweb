f76ca5bd0e3f5267f3088844166c44b290c7062b
sankarviacode <sivaaadi96@gmail.com>
2025-10-14 12:45:56 +0530

    admin edit banner

--- src/hooks/tools.ts
+++ src/hooks/tools.ts
@@ -1,5 +1,6 @@
-export const API_URL='https://ironapi.onrender.com/api/v1'
+// export const API_URL='https://ironapi.onrender.com/api/v1'
 // export const API_URL='http://localhost:5000/api/v1'
+export const API_URL='https://ironapi-xi.vercel.app/api/v1'
 
 
 // Z2WMJ2ZKK7U749F6D4TA2291
